8d0452cc88d6ef1eff6fdec8ff0d4c33170741e6
DeemaKnyazh <deemakn@yahoo.ca>
2023-10-08 11:36:09 -0400

    Add project files.

---
+++ Assi2-LH2084-DKnyazh/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Assi2_LH2084_DKnyazh.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ Assi2-LH2084-DKnyazh/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Assi2_LH2084_DKnyazh.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
