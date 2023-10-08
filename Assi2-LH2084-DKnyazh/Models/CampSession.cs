c110af7b21335a9fd5242286f5029a1ddecbdbd5
DeemaKnyazh <deemakn@yahoo.ca>
2023-10-08 11:39:14 -0400

    Created session and camper models

---
+++ Assi2-LH2084-DKnyazh/Models/CampSession.cs
@@ -0,0 +1,15 @@
+﻿namespace COMP2084_Assignment2_DmitryKnyazhevskiy.Models
+{
+    public class CampSession
+    {
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public int maxCampers { get; set; }
+
+        public int sessionId { get; set; }
+
+        public int numberCampers { get; set; }
+    }
+}
---
+++ Assi2-LH2084-DKnyazh/Models/Camper.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP2084_Assignment2_DmitryKnyazhevskiy.Models
+{
+    public class Camper
+    {
+        public int camperId { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        [MaxLength(50)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        [MaxLength(50)]
+        public string LastName { get; set; }
+
+        [Required]
+        public int age { get; set; }
+
+        [Required]
+        public int sessionId { get; set; }
+
+        [Required]
+        public int status { get; set; }
+
+    }
+}
