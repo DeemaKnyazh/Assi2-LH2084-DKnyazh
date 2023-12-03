9144a32d1ab7f7313a3eee255e3ec4e40babf066
DeemaKnyazh <deemakn@yahoo.ca>
2023-12-03 15:27:38 -0500

    Created test project and test class for the camp sessions controller

---
+++ Assi4-Testing-DmitryKnyazhevskiy/CampSessionsControllerTests.cs
@@ -0,0 +1,11 @@
+namespace Assi4_Testing_DmitryKnyazhevskiy
+{
+    [TestClass]
+    public class CampSessionsControllerTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
